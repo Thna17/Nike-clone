93ea97b8ae0dc093d893423f2b0739ec7ef83c0f
Hong Than brathna <Hangbrathna10@gmail.com>
2024-05-23 13:12:10 +0700

    add: develop hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,15 +48,18 @@ module.exports = {
         mob: '600px'
       },
       fontFamily: {
-<<<<<<< HEAD
-        Futura: 'Futura',
-        HelveticaNowText_Regular: 'HelveticaNowText-Regular',
-        HelveticaNowText_Medium: 'HelveticaNowText-Medium'
-=======
         Futura: 'Futura'
->>>>>>> 972859f (add: develop hero component)
       }
     },
+    screens: {
+      tab: '960px',
+      mob: '600px'
+    },
+    fontFamily: {
+      Futura: 'Futura',
+      HelveticaNowText_Regular: 'HelveticaNowText-Regular',
+      HelveticaNowText_Medium: 'HelveticaNowText-Medium'
+    }
   },
   plugins: [  
     require('tailwindcss'),
