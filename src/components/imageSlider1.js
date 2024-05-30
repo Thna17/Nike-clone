159a96ebf5140494bc1c56f0e4edbd6a0d2cb0c4
Hong Than brathna <Hangbrathna10@gmail.com>
2024-05-30 13:44:43 +0700

    finish: develop kids page 100%

--- src/components/imageSlider1.js
+++ src/components/imageSlider1.js
@@ -7,15 +7,9 @@ class ImageSlider extends HTMLElement {
         const images = JSON.parse(this.getAttribute('images'));
 
         this.innerHTML = `
-<<<<<<< HEAD
             <div class="flex w-[1920px] overflow-x-scroll  gap-[10px]">
                 ${images.map(imgUrl => `
                     <div class="min-w-[400px] max-w-[700px]">
-=======
-            <div class="flex w-screen overflow-x-auto gap-[10px]">
-                ${images.map(imgUrl => `
-                    <div class="min-w-[300px] max-w-[700px]">
->>>>>>> origin/feature/kids-page
                         <img src="${imgUrl}" alt="" class="w-full">
                     </div>
                 `).join('')}
